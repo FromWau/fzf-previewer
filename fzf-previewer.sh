59b5536d93fddb7f7620344c80ba283c224a9664
FromWau <tobias.frommhund@aon.at>
2023-06-01 22:34:19 +0200

    fix

--- fzf-previewer.sh
+++ fzf-previewer.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 UB_PID_FILE="/tmp/.$(uuidgen)"
-ueberzugpp layer --no-stdin --silent --use-escape-codes --pid-file "$UB_PID_FILE"
-UB_PID=$(cat "$UB_PID_FILE")
+ueberzugpp layer --no-stdin --silent --use-escape-codes --pid-file $UB_PID_FILE
+UB_PID=$(cat $UB_PID_FILE)
 
-export SOCKET="/tmp/ueberzugpp-$UB_PID.socket"
+export SOCKET=/tmp/ueberzugpp-$UB_PID.socket
 export X=$(($(tput cols) / 2 + 1))
 
 # run fzf with preview
-fzf --preview 'set file {}; if string match -r ".*\.(png|jpg|jpeg)$" $file; ueberzugpp cmd -s $SOCKET -i fzfpreview -a add -x $X -y 1 --max-width $FZF_PREVIEW_COLUMNS --max-height $FZF_PREVIEW_LINES -f $file; else bat --color always $file; end' --reverse "$@"
+fzf --preview='
+if string match -q -r ".*\.(png|jpg|jpeg)\$" {}
+    ueberzugpp cmd -s $SOCKET -i fzfpreview -a add -x $X -y 1 --max-width $FZF_PREVIEW_COLUMNS --max-height $FZF_PREVIEW_LINES -f {}
+else
+    ueberzugpp cmd -s $SOCKET -a remove
+    bat --color always {}
+end
+' "$@"
 
-ueberzugpp cmd -s "$SOCKET" -a exit
+ueberzugpp cmd -s $SOCKET -a exit
